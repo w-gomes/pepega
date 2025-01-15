b5519d552bbf2a950c6f245cbefe359e69b0b810
w-gomes <w.gomes5521250@gmail.com>
2025-01-15 13:52:54 -0300

    initial command line parser with clap

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,73 @@
 use clap::{Parser, Subcommand};
 
-use std::process::Command;
+use std::{path::PathBuf, process::Command};
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Cli {
+#[command(about = "Smol ffmpeg wrapper")]
+#[command(version, long_about = None)]
+struct Pepega {
+    /// Inputs files.
+    #[arg(short, required = true)]
+    inputs: Vec<PathBuf>,
+
+    /// Output file.
+    #[arg(short, required = true)]
+    output: PathBuf,
+
+    /// Options for the program.
     #[command(subcommand)]
-    cmd: Commands,
+    command: Commands,
 }
 
-#[derive(Subcommand, Debug, Clone)]
+#[derive(Subcommand, Debug)]
 enum Commands {
-    Get {
-        #[arg(required = true)]
-        value: String,
+    /// Creates a clip of video with given start and end
+    Clip { start: String, end: String },
+
+    /// Merges two or more videos
+    Merge,
+
+    /// Creates a video from images with default framaterate to 1/5
+    /// If desired, user can pass a new value for framerate between 0 and 5
+    Video {
+        #[arg(short, value_name = "FRAMERATE")]
+        framerate: Option<u16>,
     },
-    Set {
-        #[arg(required = true)]
-        value: String,
+
+    /// Extracts an audio stream from a video
+    Audio,
+
+    /// Encodes a video with default value for crf of 23
+    /// If desired, user can pass a new value for crf between 0 and 51
+    Encode {
+        #[arg(short, value_name = "CRF")]
+        crf: Option<u16>,
     },
+
+    /// Encodes a video for youtube
+    Youtube,
 }
 
-fn main() {
-    //let args = Cli::parse();
-
-    //let mut v = "hello".to_string();
-
-    //match args.cmd {
-    //    Commands::Get { ref value } => {
-    //        println!("Getting {value}");
-    //        if *value == v {
-    //            println!("Here's the value {v}");
-    //        } else {
-    //            println!("This value doesn't exist.");
-    //        }
-    //    }
-
-    //    Commands::Set { ref value } => {
-    //        println!("Setting value to {value}");
-    //        println!("Old value {v}");
-    //        v = value.clone();
-    //        println!("New value {v}");
-    //    }
-    //}
-
+fn run_ffmpeg(arg: &str) {
     let output = Command::new("ffmpeg")
-        .arg("-version")
+        .arg(arg)
         .output()
         .expect("Failed to execute command");
 
-    println!("{}", String::from_utf8_lossy(&output.stdout))
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+}
+
+fn main() {
+    let args = Pepega::parse();
+
+    match args.command {
+        Commands::Clip { start, end } => {
+            run_ffmpeg("-version");
+        }
+        Commands::Merge => {}
+        Commands::Video { framerate } => {}
+        Commands::Audio => {}
+        Commands::Encode { crf } => {}
+        Commands::Youtube => {}
+    }
 }
