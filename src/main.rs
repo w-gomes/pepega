2599ff825b88cec9b8822c7ebcd502d1be506345
w-gomes <w.gomes5521250@gmail.com>
2025-01-16 13:07:45 -0300

    almost everything is done. need test

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use clap::{Parser, Subcommand};
 use std::process::Command;
 
 #[derive(Parser, Debug)]
-#[command(about = "Smol ffmpeg wrapper")]
+#[command(about = "Smol video tool that uses ffmpeg under the hood.")]
 #[command(version, long_about = None)]
-struct Pepega {
+struct Yuh {
     /// Inputs files.
     #[arg(short, required = true)]
     inputs: Vec<String>,
@@ -22,27 +22,27 @@ struct Pepega {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// Creates a clip of video with given start and end
+    /// Creates a clip of video with given START and END
     Clip { start: String, end: String },
 
     /// Merges two or more videos
     Merge,
 
-    /// Creates a video from images with default framaterate to 1/5
-    /// If desired, user can pass a new value for framerate between 0 and 5
+    /// Creates a video from images with default framaterate of 1/5.
+    /// If desired, user can pass a new value for the framerate BETWEEN 0 and 5
     Video {
         #[arg(short, value_name = "FRAMERATE")]
-        framerate: Option<u16>,
+        framerate: Option<i16>,
     },
 
-    /// Extracts an audio stream from a video
+    /// Extracts the audio stream from a video
     Audio,
 
-    /// Encodes a video with default value for crf of 23
+    /// Encodes a video with default value of 23 for crf.
     /// If desired, user can pass a new value for crf between 0 and 51
     Encode {
         #[arg(short, value_name = "CRF")]
-        crf: Option<u16>,
+        crf: Option<i16>,
     },
 
     /// Encodes a video for youtube
@@ -74,7 +74,7 @@ fn run_ffmpeg(args: &[String]) {
 }
 
 fn main() {
-    let args = Pepega::parse();
+    let args = Yuh::parse();
 
     match args.command {
         Commands::Clip { start, end } => {
@@ -104,6 +104,9 @@ fn main() {
             }
         }
         Commands::Merge => {
+            //TODO: Fix this
+            println!("Not implemented!");
+            /*
             // we expect more TWO or MORE inputs.
             if args.inputs.len() < 2 {
                 eprintln!("Not enough inputs for this command!");
@@ -120,6 +123,7 @@ fn main() {
                 // maybe figure something out better than intersperse.
                 merge_args.insert(0, String::from("-i"));
 
+                merge_args.push(String::from("-y"));
                 merge_args.push(String::from("-vcodec"));
                 merge_args.push(String::from("copy"));
                 merge_args.push(String::from("-acodec"));
@@ -129,11 +133,143 @@ fn main() {
                 merge_args.push(format!("{output}"));
                 run_ffmpeg(&merge_args);
             }
+            */
         }
 
-        Commands::Video { framerate } => {}
-        Commands::Audio => {}
-        Commands::Encode { crf } => {}
-        Commands::Youtube => {}
+        Commands::Video { framerate } => {
+            if args.inputs.len() > 1 {
+                // TODO: check this.
+                // Actually we only need a path to the folder containing the images.
+                eprintln!("We only need the pattern.");
+            } else {
+                let framerate = match framerate {
+                    Some(framerate) => {
+                        if framerate < 1 && framerate > 10 {
+                            println!(
+                                "framerate ({}) value out of range [1..10]. Defaulting to 5.",
+                                framerate
+                            );
+                            5
+                        } else {
+                            framerate
+                        }
+                    }
+                    None => 5,
+                };
+
+                let mut video_args = Vec::new();
+                let input = args.inputs[0].clone();
+                let output = args.output.clone();
+
+                video_args.push(String::from("-y"));
+                video_args.push(String::from("-framerate"));
+                video_args.push(format!("-1/{framerate}"));
+                video_args.push(String::from("-pattern_type"));
+                video_args.push(String::from("glob"));
+                video_args.push(String::from("-i"));
+                video_args.push(format!("'{input}'"));
+                video_args.push(String::from("-c:v"));
+                video_args.push(String::from("libx264"));
+                video_args.push(String::from("-r"));
+                video_args.push(String::from("30"));
+                video_args.push(String::from("-pix_fmt"));
+                video_args.push(String::from("-yuv420p"));
+                video_args.push(format!("{output}"));
+
+                run_ffmpeg(&video_args);
+            }
+        }
+        Commands::Audio => {
+            // we only expect ONE input.
+            if args.inputs.len() > 1 {
+                eprintln!("Too many inputs for this command!");
+            } else {
+                let input = args.inputs[0].clone();
+                let output = args.output.clone();
+
+                let mut audio_args = Vec::new();
+
+                // overwrites file if it already exists.
+                audio_args.push(String::from("-y"));
+                audio_args.push(String::from("-i"));
+                audio_args.push(format!("{input}"));
+                audio_args.push(String::from("-vn"));
+                audio_args.push(String::from("-c:a"));
+                audio_args.push(String::from("mp3"));
+                audio_args.push(format!("{output}"));
+
+                run_ffmpeg(&audio_args);
+            }
+        }
+        Commands::Encode { crf } => {
+            // we only expect ONE input.
+            if args.inputs.len() > 1 {
+                eprintln!("Too many inputs for this command!");
+            } else {
+                let crf = match crf {
+                    Some(crf) => {
+                        if crf < 0 && crf > 51 {
+                            println!(
+                                "crf ({}) value out of range [0..51]. Defaulting to 23.",
+                                crf
+                            );
+                            23
+                        } else {
+                            crf
+                        }
+                    }
+                    None => 23,
+                };
+
+                let input = args.inputs[0].clone();
+                let output = args.output.clone();
+
+                let mut encode_args = Vec::new();
+
+                // overwrites file if it already exists.
+                encode_args.push(String::from("-y"));
+                encode_args.push(String::from("-i"));
+                encode_args.push(format!("{input}"));
+                encode_args.push(String::from("-c:v"));
+                encode_args.push(String::from("-libx264"));
+                encode_args.push(String::from("-crf"));
+                encode_args.push(format!("{crf}"));
+                encode_args.push(String::from("-c:a"));
+                encode_args.push(String::from("copy"));
+                encode_args.push(format!("{output}"));
+
+                run_ffmpeg(&encode_args);
+            }
+        }
+        Commands::Youtube => {
+            // we only expect ONE input.
+            if args.inputs.len() > 1 {
+                eprintln!("Too many inputs for this command!");
+            } else {
+                let input = args.inputs[0].clone();
+                let output = args.output.clone();
+
+                let mut youtube_args = Vec::new();
+
+                // overwrites file if it already exists.
+                youtube_args.push(String::from("-y"));
+                youtube_args.push(String::from("-i"));
+                youtube_args.push(format!("{input}"));
+                youtube_args.push(String::from("-c:v"));
+                youtube_args.push(String::from("libx264"));
+                youtube_args.push(String::from("-crf"));
+                youtube_args.push(String::from("18"));
+                youtube_args.push(String::from("-preset"));
+                youtube_args.push(String::from("-c:a"));
+                youtube_args.push(String::from("aac"));
+                youtube_args.push(String::from("-b:a"));
+                youtube_args.push(String::from("384k"));
+                youtube_args.push(String::from("-pix_fmt"));
+                youtube_args.push(String::from("yuv420p"));
+                youtube_args.push(format!("{output}"));
+
+                run_ffmpeg(&youtube_args);
+            }
+        }
     }
 }
